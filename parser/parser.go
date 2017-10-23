b145f122564d894e3a6fe7e3ed79d6f8252c453b
VitaliiMichailovich <vitaliy.michailovich@gmail.com>
2017-10-23 15:19:50 +0300

    Fix parser (without implementation).

--- parser/parser.go
+++ parser/parser.go
@@ -2,51 +2,46 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"golang.org/x/net/html"
 	"net/http"
-	"strconv"
 	"strings"
 	"sync"
 	"time"
 )
 
-type Link struct {
+type link struct {
 	Count   int
 	Checked bool
 	Routine bool
 }
 
-var Links = make(map[string]Link)
+var links = make(map[string]link)
 
-var mutex = &sync.Mutex{}
+var mutex = &sync.RWMutex{}
 
 var wg = sync.WaitGroup{}
 
-//var ch = make(chan struct{}, 1000)
-
-func Parser(uri string) (links map[string]Link, err error) {
+func Parser(uri string) (map[string]link, error) {
 	mutex.Lock()
-	Links[uri] = Link{Count: 1, Checked: false, Routine: false}
+	links[uri] = link{Count: 1, Checked: false, Routine: false}
 	mutex.Unlock()
 	for {
-		mutex.Lock()
-		li := make(map[string]Link, len(Links))
-		for key, value := range Links {
+		mutex.RLock()
+		li := make(map[string]link, len(links))
+		for key, value := range links {
 			li[key] = value
 		}
-		mutex.Unlock()
+		mutex.RUnlock()
 		var checked, runned, run int
 		for k, v := range li {
 			if v.Checked == false {
 				if v.Routine == false {
 					mutex.Lock()
-					Links[k] = Link{Count: Links[k].Count, Checked: Links[k].Checked, Routine: true}
+					links[k] = link{Count: links[k].Count, Checked: links[k].Checked, Routine: true}
 					mutex.Unlock()
 					run++
 					wg.Add(1)
-					//ch <- struct{}{}
 					go linksWriter(k, uri)
 				} else {
 					runned++
@@ -56,63 +51,66 @@ func Parser(uri string) (links map[string]Link, err error) {
 			}
 		}
 		time.Sleep(time.Second * 1)
-		mutex.Lock()
-		summ := len(Links)
-		mutex.Unlock()
+		mutex.RLock()
+		summ := len(links)
+		mutex.RUnlock()
 		fmt.Printf("Total links:\t%v\t\tChecked:\t%v\t\tRunning:\t%v\t\tRuned:\t%v\n", summ, checked, runned, run)
-		if summ == 3992 && checked == 3590 && runned == 402 && run == 0 {
-			break
-		}
-		if summ == checked-1 {
-			mutex.Lock()
-			fmt.Println("Links len 0 : " + strconv.Itoa(len(Links)))
-			mutex.Unlock()
-			time.Sleep(time.Second * 2)
-		}
+		//if summ == 3998 && checked == 3596 && runned == 402 && run == 0 {
+		//	// Need to solve this problem
+		//	mutex.RLock()
+		//	for k, v := range Links {
+		//		if v.Checked == false && v.Routine == true {
+		//			fmt.Println(k)
+		//		}
+		//	}
+		//	mutex.RUnlock()
+		//	break
+		//}
+		//if summ == checked+1 {
+		//	mutex.RLock()
+		//	fmt.Println("Links len 0 : " + strconv.Itoa(len(Links)))
+		//	mutex.RUnlock()
+		//	time.Sleep(time.Second * 2)
+		//}
 		if summ == checked {
-			mutex.Lock()
-			fmt.Println("Links len 1 : " + strconv.Itoa(len(Links)))
-			mutex.Unlock()
+			//mutex.RLock()
+			//fmt.Println("Links len 1 : " + strconv.Itoa(len(Links)))
+			//mutex.RUnlock()
 			break
 		}
 	}
 	wg.Wait()
-	mutex.Lock()
-	fmt.Println("Links len 2 : " + strconv.Itoa(len(Links)))
-	for k, v := range Links {
-		fmt.Println(k, v)
-	}
-	links = make(map[string]Link)
-	err = errors.New("AAA")
-	mutex.Unlock()
-	return
+	//mutex.RLock()
+	//for k, v := range Links {
+	//	fmt.Println(k, v)
+	//}
+	//mutex.RUnlock()
+	return links, nil
 }
 
-func linksWriter(link, uri string) {
+func linksWriter(linka, uri string) {
 	defer wg.Done()
-	response, err := http.Get(link)
-	defer recover()
+	response, err := http.Get(linka)
 	if err != nil {
-		fmt.Println(err.Error())
+		//fmt.Println(err.Error())
 		mutex.Lock()
-		Links[link] = Link{Checked: false, Count: Links[link].Count, Routine: false}
+		links[linka] = link{Checked: false, Count: links[linka].Count, Routine: false}
 		mutex.Unlock()
-
 		return
 	}
 	defer response.Body.Close()
 	if response.StatusCode >= 501 {
 		mutex.Lock()
-		Links[link] = Link{Checked: false, Count: Links[link].Count, Routine: false}
+		links[linka] = link{Checked: false, Count: links[linka].Count, Routine: false}
 		mutex.Unlock()
 		response.Body.Close()
 		return
 	}
 	if response.StatusCode != http.StatusOK {
 		mutex.Lock()
-		Links[link] = Link{Checked: true, Count: Links[link].Count, Routine: false}
+		links[linka] = link{Checked: true, Count: links[linka].Count, Routine: false}
 		mutex.Unlock()
-		fmt.Println("Not Status OK -------------------------- ", response.StatusCode, link)
+		//fmt.Println("Not Status OK -------------------------- ", response.StatusCode, link)
 		response.Body.Close()
 		return
 	}
@@ -136,12 +134,17 @@ func linksWriter(link, uri string) {
 						continue top
 					}
 					mutex.Lock()
-					Links[url] = Link{Count: Links[url].Count + 1, Checked: Links[url].Checked, Routine: Links[url].Routine}
+					links[url] = link{Count: links[url].Count + 1, Checked: links[url].Checked, Routine: links[url].Routine}
 					mutex.Unlock()
 				}
 			}
 			mutex.Lock()
-			Links[link] = Link{Checked: true, Count: Links[link].Count, Routine: false}
+			links[linka] = link{Checked: true, Count: links[linka].Count, Routine: false}
+			mutex.Unlock()
+		} else {
+
+			mutex.Lock()
+			delete(links, linka)
 			mutex.Unlock()
 		}
 	}
@@ -178,9 +181,9 @@ func getHref(t html.Token, uri string) (ok bool, href string) {
 				last4 == ".txt" ||
 				last4 == ".rtf" ||
 				last4 == ".jpg" ||
-				last4 == ".jpeg" ||
-				last4 == ".docx" ||
-				last4 == ".xlsx" {
+				last4 == "jpeg" ||
+				last4 == "docx" ||
+				last4 == "xlsx" {
 				return false, ""
 			}
 			ok = true
@@ -192,58 +195,3 @@ func getHref(t html.Token, uri string) (ok bool, href string) {
 func main() {
 	Parser("http://library.dp.ua/")
 }
-
-//
-//func ParserA(uri string) error {
-//	var link string
-//	for k, v := range Links {
-//		if v.Checked == false {
-//			link = k
-//			break
-//		}
-//	}
-//afterSleep:
-//	time.Sleep(1 * time.Millisecond)
-//	response, err := http.Get(link)
-//	if response.StatusCode == 503 {
-//		time.Sleep(10 * time.Second)
-//		//fmt.Println("503 : ", link)
-//		goto afterSleep
-//	}
-//	if err == nil && response.StatusCode == 200 {
-//		defer response.Body.Close()
-//		if response.Header["Content-Type"][0] == "text/html" {
-//			tokenizer := html.NewTokenizer(response.Body)
-//		top:
-//			for {
-//				linkInfo := tokenizer.Next()
-//				switch {
-//				case linkInfo == html.ErrorToken:
-//					break top
-//				case linkInfo == html.StartTagToken:
-//					tagInfo := tokenizer.Token()
-//					isAnchor := tagInfo.Data == "a"
-//					if !isAnchor {
-//						continue top
-//					}
-//					ok, url := getHref(tagInfo, uri)
-//					if !ok {
-//						fmt.Println(len(Links), "/", trues, " - ", response.StatusCode, " - ", link, " - ", tagInfo)
-//						continue top
-//					}
-//					Links[url] = Link{Count: Links[url].Count + 1, Checked: Links[url].Checked}
-//				}
-//			}
-//		}
-//	}
-//	Links[link] = Link{Checked: true, Count: Links[link].Count}
-//	trues++
-//	if trues%1000 == 0 {
-//		fmt.Println(len(Links), "/", trues, " - ", response.StatusCode, " - ", link)
-//	}
-//
-//	if len(Links) != trues {
-//		ParserA(uri)
-//	}
-//	return nil
-//}
